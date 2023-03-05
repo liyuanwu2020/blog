3cb7f905f712756f6827609775ba8b8e051b513d
liyuanwu <16619891743@163.com>
2023-03-05 23:13:21 +0800

    html template

--- main.go
+++ main.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/liyuanwu2020/msgo"
+	"html/template"
 	"log"
+	"time"
 )
+
+type tplData struct {
+	Title string
+}
+
+func ShowTime() string {
+	return time.Now().String()
+}
 
 func main() {
 	log.Println("main start")
@@ -22,7 +32,16 @@ func main() {
 	//}
 
 	engine := msgo.New()
+	funcMap := template.FuncMap{"showTime": ShowTime}
+	engine.SetFuncMap(funcMap)
+	engine.LoadTemplate("tpl/*.html")
 	g := engine.Group("user")
+
+	g.Get("/home", func(ctx *msgo.Context) {
+		err := ctx.Template("index.html", &tplData{Title: "个人中心"})
+		log.Println(err)
+	})
+
 	g.Use(func(handlerFunc msgo.HandlerFunc) msgo.HandlerFunc {
 		return func(ctx *msgo.Context) {
 			log.Println("test preHandler")
@@ -43,6 +62,14 @@ func main() {
 			log.Println("test router middle")
 			handlerFunc(ctx)
 		}
+	})
+	g.Get("/index", func(ctx *msgo.Context) {
+		err := ctx.HTMLTemplate("index.html", &tplData{Title: "首页"}, "tpl/index.html", "tpl/header.html")
+		log.Println(err)
+	})
+	g.Get("/login", func(ctx *msgo.Context) {
+		err := ctx.HTMLTemplateGlob("login.html", &tplData{Title: "登录页"}, "tpl/*.html")
+		log.Println(err)
 	})
 	//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
 	//git remote add origin https://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@github.com/liyuanwu2020/msgo.git

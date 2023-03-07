7cf131d93eb80faa80f2bd138f2e7d7c7616522e
好好住-liyuanwu <liyuanwu@haohaozhu.com>
2023-03-07 19:02:18 +0800

    support json

--- main.go
+++ main.go
@@ -12,20 +12,19 @@ func ShowTime() string {
 	return time.Now().String()
 }
 
-func main() {
-	log.Println("main start")
+type User struct {
+	Name    string   `json:"name" required:"true"`
+	Age     int      `json:"age"`
+	Address []string `json:"address"`
+	Class   string   `json:"class"`
+}
 
-	//http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-	//	_, err := fmt.Fprintf(w, "%s 欢迎来到马神之路goweb教程", "lyw.com")
-	//	log.Println("HandleFunc ", err)
-	//	if err != nil {
-	//		return
-	//	}
-	//})
-	//err := http.ListenAndServe(":8088", nil)
-	//if err != nil {
-	//	log.Fatal("启动失败", err)
-	//}
+func main() {
+	//1.创建引擎 (创建上下文)
+	//2.添加模板函数 && 解析模板
+	//3.使用引擎创建组
+	//4.使用组创建路由
+	//5.启动引擎
 
 	engine := msgo.New()
 	funcMap := template.FuncMap{"showTime": ShowTime}
@@ -42,12 +41,28 @@ func main() {
 		log.Println("重定向不执行?")
 	})
 
-	g.Get("/home", func(ctx *msgo.Context) {
+	g.Any("/home", func(ctx *msgo.Context) {
 		var err error
-		query, _ := ctx.GetMapQuery("user")
+		user := make([]*User, 0)
+		ctx.IsValidate = true
+		err = ctx.DealJson(&user)
+		if err != nil {
+			log.Println("处理JSON错误", err)
+		}
+		//query, _ := ctx.GetAllPost()
+		//log.Println(query)
+		//avatar, fErr := ctx.FormFile("avatar")
+		//if fErr != nil {
+		//	log.Println("获取上传文件错误", err)
+		//}
+		//err = ctx.SaveUploadedFile(avatar, "./upload/"+avatar.Filename)
+		//if err != nil {
+		//	log.Println("上传文件错误", err)
+		//}
+
 		//err = ctx.HTML(http.StatusOK, "<h2>HTML</h2>")
 		//err = ctx.Template("index.html", &tplData{Title: "个人中心"})
-		err = ctx.JSON(http.StatusOK, query)
+		err = ctx.JSON(http.StatusOK, user)
 		//err = ctx.XML(http.StatusOK, &tplData{Title: "个人中心", Age: 20})
 		//ctx.File("tpl/2023课程表.xlsx")
 		//ctx.FileAttachment("tpl/2023课程表.xlsx", "myCourse.xlsx")
@@ -71,44 +86,6 @@ func main() {
 			//log.Println("组级别 PostMiddleHandler")
 		}
 	})
-
-	//g.Post("/account/:id/edit", func(ctx *msgo.Context) {
-	//	log.Println("exec main start")
-	//	_, err := fmt.Fprintf(ctx.W, "%s 欢迎来到马神之路goweb教程", "lyw.com")
-	//	if err != nil {
-	//		return
-	//	}
-	//	log.Println("exec main end")
-	//}, func(handlerFunc msgo.HandlerFunc) msgo.HandlerFunc {
-	//	return func(ctx *msgo.Context) {
-	//		log.Println("test router middle")
-	//		handlerFunc(ctx)
-	//	}
-	//})
-	//g.Get("/index", func(ctx *msgo.Context) {
-	//	err := ctx.HTMLTemplate("index.html", &tplData{Title: "首页"}, "tpl/index.html", "tpl/header.html")
-	//	log.Println(err)
-	//})
-	//g.Get("/login", func(ctx *msgo.Context) {
-	//	err := ctx.HTMLTemplateGlob("login.html", &tplData{Title: "登录页"}, "tpl/*.html")
-	//	log.Println(err)
-	//})
-	//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
-	//git remote add origin https://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@github.com/liyuanwu2020/msgo.git
-	//g.Post("/hello/:id/edit", func(ctx *msgo.Context) {
-	//	_, err := fmt.Fprintf(ctx.W, "%s Post 欢迎来到马神之路goweb教程", "lyw.com")
-	//	log.Println("HandleFunc ", err)
-	//	if err != nil {
-	//		return
-	//	}
-	//})
-	//g.Get("/hello", func(ctx *msgo.Context) {
-	//	_, err := fmt.Fprintf(ctx.W, "%s get 欢迎来到马神之路goweb教程", "lyw.com")
-	//	log.Println("HandleFunc ", err)
-	//	if err != nil {
-	//		return
-	//	}
-	//})
-
+	log.Println("engine start")
 	engine.Run()
 }
